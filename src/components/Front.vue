39e8a67496ca2a8d236bf09013b019540df0a581
Chris <chris.kieran@gmail.com>
2020-02-15 15:03:39 -0500

    static daytime content

---
+++ src/components/Front.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="front">
+    <Sign />
+    <div class="row">
+        <div class="col-4 front-left">
+            <Window />
+        </div>
+        <div class="col-4 door">
+            <Door />
+        </div>
+        <div class="col-4 front-right">
+            <Window />
+            <Mailbox />
+        </div>
+    </div>
+</div>
+    
+</template>
+
+<script>
+import Window from './Window.vue'
+import Sign from './Sign.vue'
+import Door from './Door.vue'
+import Mailbox from './Mailbox.vue'
+
+export default {
+    components: {
+    Window,
+    Sign,
+    Door,
+    Mailbox
+  }
+}
+</script>
+
+<style scoped>
+.front {
+    width: 320px;
+    margin: auto;
+    height: 200px;
+    background: #cccccc;
+    position: relative;
+}
+.front-left {
+    padding-right: 0;
+}
+.front-right {
+    padding-left: 0;
+}
+.door {
+    padding-left: 12px;
+    padding-right: 12px;
+}
+</style>
---
+++ src/components/Mailbox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mailbox">
+        <div class="mailslot"></div>
+        <p>Mailbox</p>
+    </div>
+</template>
+
+<style scoped>
+.mailbox {
+    height: 50px;
+    width: 70px;
+    background: #999999;
+    margin-top: 26px;
+    margin-left: 13px;
+    padding-top: 10px;
+}
+.mailbox p {
+    color: #666666;
+    font-size: 75%;
+    text-align: center;
+    font-weight: bold;
+}
+.mailslot {
+    background: #666666;
+    height: 8px;
+    width: 50px;
+    margin: auto;
+}
+</style>
