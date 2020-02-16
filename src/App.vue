93a87e8abdd16905b9cfd9495ecb4f5ccd6fd8e6
Chris <chris.kieran@gmail.com>
2020-02-15 20:59:39 -0500

    setup vuex and get first day-night toggle working

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app" class="container">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div id="app" 
+    class="container" 
+    v-bind:class="{
+      day: this.$store.state.isDaytime, 
+      night: !this.$store.state.isDaytime}">
     <Sky />
     <Building />
   </div>
@@ -10,15 +12,28 @@
 <script>
 import Sky from './components/Sky.vue'
 import Building from './components/Building.vue'
+import {store} from './store.js';
 
 export default {
   name: 'App',
   components: {
     Sky,
     Building
+  },
+  computed: {
+      isDaytime() {
+          return store.state.isDaytime;
+      }
   }
 }
 </script>
 
 <style>
+.container {
+  padding-top: 10em;
+  min-height: 100vh;
+}
+.night {
+  background-color: midnightblue;
+}
 </style>
