e978ca4407f3eff385e9071320e6f2fb25064115
Chris <chris.kieran@gmail.com>
2020-02-15 21:42:18 -0500

    window cookie toggle working

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,15 +1,39 @@
 <template>
-    <div class="window"></div>
+    <div v-bind:class="{
+      day: this.$store.state.isDaytime, 
+      night: !this.$store.state.isDaytime}">
+    </div>
 </template>
 
-<style scoped>
-    .window {
-        height: 70px;
-        width: 70px;
-        border-radius: 50%;
-        background: #cfe2f3;
-        margin: auto;
-        margin-top: 20px;
-        border: 3px solid #9fc5e8;
+<script>
+import {store} from '../store.js';
+
+export default {
+    computed: {
+        isDaytime() {
+            return store.state.isDaytime;
+        }
     }
+}
+</script>
+
+<style scoped>
+.day, .night {
+    height: 70px;
+    width: 70px;
+    border-radius: 50%;
+    margin: auto;
+    margin-top: 20px;
+    border: 3px solid #9fc5e8;
+}
+.day {
+    background-color: #cfe2f3; 
+}
+.day:hover {
+    background-image: url('../assets/cookie.jpg');
+    background-size: cover;
+}
+.night {
+    background: grey; 
+}
 </style>
